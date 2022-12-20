cce2e52a3f4fd22f0e1d2445c94a37876e2ef604
Henrik Berglund <110549389+henrikvilhelmberglund@users.noreply.github.com>
2022-12-20 13:13:14 +0100

    feat: :sparkles: add svelte-fa

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,13 @@
+<script>
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<i class="fas fa-pen"></i>
+
+<style>
+	p {
+		font-size: 1.5rem;
+	}
+</style>
