80b670a2230c9ce38d923deaa70c8f8f954c4aa7
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-01-04 17:50:10 +0100

    feat: :sparkles: add color families

---
+++ src/lib/Colors.svelte
@@ -0,0 +1,420 @@
+<h1 class="font-bold text-lg">Colors</h1>
+<h2>Cornflower Blue</h2>
+<!-- 
+'cornflower-blue': {
+    '50': '#f1f6fd',
+    '100': '#dfebfa',
+    '200': '#c6dcf7',
+    '300': '#9fc5f1',
+    '400': '#6da3e8',
+    '500': '#5086e1',
+    '600': '#3b6ad5',
+    '700': '#3257c3',
+    '800': '#2e489f',
+    '900': '#2a3f7e',
+},
+
+ -->
+<svg
+	width="1000"
+	height="100"
+	viewBox="0 0 1000 100"
+	fill="none"
+	xmlns="http://www.w3.org/2000/svg"
+>
+	<rect width="100" height="100" fill="#f1f6fd" x="0" y="0" />
+	<rect width="100" height="100" fill="#dfebfa" x="100" y="0" />
+	<rect width="100" height="100" fill="#c6dcf7" x="200" y="0" />
+	<rect width="100" height="100" fill="#9fc5f1" x="300" y="0" />
+	<rect width="100" height="100" fill="#6da3e8" x="400" y="0" />
+	<rect width="100" height="100" fill="#5086e1" x="500" y="0" />
+	<rect width="100" height="100" fill="#3b6ad5" x="600" y="0" />
+	<rect width="100" height="100" fill="#3257c3" x="700" y="0" />
+	<rect width="100" height="100" fill="#2e489f" x="800" y="0" />
+	<rect width="100" height="100" fill="#2a3f7e" x="900" y="0" />
+</svg>
+
+<h2>Cornflower blue</h2>
+<!-- 
+'cornflower-blue': {
+    '50': '#eff2fe',
+    '100': '#e2e7fd',
+    '200': '#cbd2fa',
+    '300': '#abb5f6',
+    '400': '#898ef0',
+    '500': '#6f6de8',
+    '600': '#5d50db',
+    '700': '#5041c1',
+    '800': '#41379c',
+    '900': '#39337c',
+},
+
+ -->
+<svg
+	width="1000"
+	height="100"
+	viewBox="0 0 1000 100"
+	fill="none"
+	xmlns="http://www.w3.org/2000/svg"
+>
+	<rect width="100" height="100" fill="#eff2fe" x="0" y="0" />
+	<rect width="100" height="100" fill="#e2e7fd" x="100" y="0" />
+	<rect width="100" height="100" fill="#cbd2fa" x="200" y="0" />
+	<rect width="100" height="100" fill="#abb5f6" x="300" y="0" />
+	<rect width="100" height="100" fill="#898ef0" x="400" y="0" />
+	<rect width="100" height="100" fill="#6f6de8" x="500" y="0" />
+	<rect width="100" height="100" fill="#5d50db" x="600" y="0" />
+	<rect width="100" height="100" fill="#5041c1" x="700" y="0" />
+	<rect width="100" height="100" fill="#41379c" x="800" y="0" />
+	<rect width="100" height="100" fill="#39337c" x="900" y="0" />
+</svg>
+
+<h2>Medium purple</h2>
+<!-- 
+'medium-purple': {
+    '50': '#faf6fe',
+    '100': '#f2eafd',
+    '200': '#e8d9fb',
+    '300': '#d6bbf7',
+    '400': '#bc90f0',
+    '500': '#a76de8',
+    '600': '#8c45d8',
+    '700': '#7733bd',
+    '800': '#662f9a',
+    '900': '#54277c',
+},
+ -->
+<svg
+	width="1000"
+	height="100"
+	viewBox="0 0 1000 100"
+	fill="none"
+	xmlns="http://www.w3.org/2000/svg"
+>
+	<rect width="100" height="100" fill="#faf6fe" x="0" y="0" />
+	<rect width="100" height="100" fill="#f2eafd" x="100" y="0" />
+	<rect width="100" height="100" fill="#e8d9fb" x="200" y="0" />
+	<rect width="100" height="100" fill="#d6bbf7" x="300" y="0" />
+	<rect width="100" height="100" fill="#bc90f0" x="400" y="0" />
+	<rect width="100" height="100" fill="#a76de8" x="500" y="0" />
+	<rect width="100" height="100" fill="#8c45d8" x="600" y="0" />
+	<rect width="100" height="100" fill="#7733bd" x="700" y="0" />
+	<rect width="100" height="100" fill="#662f9a" x="800" y="0" />
+	<rect width="100" height="100" fill="#54277c" x="900" y="0" />
+</svg>
+
+<h2>Lavender</h2>
+<!-- 
+'lavender': {
+    '50': '#fcf5fe',
+    '100': '#faeafd',
+    '200': '#f4d5f9',
+    '300': '#edb3f4',
+    '400': '#de6de8',
+    '500': '#d357de',
+    '600': '#b938c1',
+    '700': '#9c2ba0',
+    '800': '#812583',
+    '900': '#6c236c',
+},
+ -->
+<svg
+	width="1000"
+	height="100"
+	viewBox="0 0 1000 100"
+	fill="none"
+	xmlns="http://www.w3.org/2000/svg"
+>
+	<rect width="100" height="100" fill="#fcf5fe" x="0" y="0" />
+	<rect width="100" height="100" fill="#faeafd" x="100" y="0" />
+	<rect width="100" height="100" fill="#f4d5f9" x="200" y="0" />
+	<rect width="100" height="100" fill="#edb3f4" x="300" y="0" />
+	<rect width="100" height="100" fill="#de6de8" x="400" y="0" />
+	<rect width="100" height="100" fill="#d357de" x="500" y="0" />
+	<rect width="100" height="100" fill="#b938c1" x="600" y="0" />
+	<rect width="100" height="100" fill="#9c2ba0" x="700" y="0" />
+	<rect width="100" height="100" fill="#812583" x="800" y="0" />
+	<rect width="100" height="100" fill="#6c236c" x="900" y="0" />
+</svg>
+<h2>Carissma</h2>
+<!-- 
+'carissma': {
+    '50': '#fcf3f9',
+    '100': '#fae9f5',
+    '200': '#f8d2ec',
+    '300': '#f3aedb',
+    '400': '#e86dbb',
+    '500': '#e054a9',
+    '600': '#ce3489',
+    '700': '#b2246f',
+    '800': '#93215c',
+    '900': '#7b204f',
+},
+ -->
+<svg
+	width="1000"
+	height="100"
+	viewBox="0 0 1000 100"
+	fill="none"
+	xmlns="http://www.w3.org/2000/svg"
+>
+	<rect width="100" height="100" fill="#fcf3f9" x="0" y="0" />
+	<rect width="100" height="100" fill="#fae9f5" x="100" y="0" />
+	<rect width="100" height="100" fill="#f8d2ec" x="200" y="0" />
+	<rect width="100" height="100" fill="#f3aedb" x="300" y="0" />
+	<rect width="100" height="100" fill="#e86dbb" x="400" y="0" />
+	<rect width="100" height="100" fill="#e054a9" x="500" y="0" />
+	<rect width="100" height="100" fill="#ce3489" x="600" y="0" />
+	<rect width="100" height="100" fill="#b2246f" x="700" y="0" />
+	<rect width="100" height="100" fill="#93215c" x="800" y="0" />
+	<rect width="100" height="100" fill="#7b204f" x="900" y="0" />
+</svg>
+<h2>Deep Blush</h2>
+<!-- 
+'deep-blush': {
+    '50': '#fdf3f3',
+    '100': '#fce7ea',
+    '200': '#f8d3d9',
+    '300': '#f3aeba',
+    '400': '#e86d84',
+    '500': '#df5472',
+    '600': '#ca345b',
+    '700': '#aa264c',
+    '800': '#8f2245',
+    '900': '#7b2040',
+},
+ -->
+<svg
+	width="1000"
+	height="100"
+	viewBox="0 0 1000 100"
+	fill="none"
+	xmlns="http://www.w3.org/2000/svg"
+>
+	<rect width="100" height="100" fill="#fdf3f3" x="0" y="0" />
+	<rect width="100" height="100" fill="#fce7ea" x="100" y="0" />
+	<rect width="100" height="100" fill="#f8d3d9" x="200" y="0" />
+	<rect width="100" height="100" fill="#f3aeba" x="300" y="0" />
+	<rect width="100" height="100" fill="#e86d84" x="400" y="0" />
+	<rect width="100" height="100" fill="#df5472" x="500" y="0" />
+	<rect width="100" height="100" fill="#ca345b" x="600" y="0" />
+	<rect width="100" height="100" fill="#aa264c" x="700" y="0" />
+	<rect width="100" height="100" fill="#8f2245" x="800" y="0" />
+	<rect width="100" height="100" fill="#7b2040" x="900" y="0" />
+</svg>
+<h2>Apricot</h2>
+<!-- 
+'apricot': {
+    '50': '#fdf4ef',
+    '100': '#fae4da',
+    '200': '#f3c7b5',
+    '300': '#e88e6d',
+    '400': '#e37154',
+    '500': '#dd4f32',
+    '600': '#ce3928',
+    '700': '#ab2a23',
+    '800': '#892423',
+    '900': '#6f211f',
+},
+
+ -->
+<svg
+	width="1000"
+	height="100"
+	viewBox="0 0 1000 100"
+	fill="none"
+	xmlns="http://www.w3.org/2000/svg"
+>
+	<rect width="100" height="100" fill="#fdf4ef" x="0" y="0" />
+	<rect width="100" height="100" fill="#fae4da" x="100" y="0" />
+	<rect width="100" height="100" fill="#f3c7b5" x="200" y="0" />
+	<rect width="100" height="100" fill="#e88e6d" x="300" y="0" />
+	<rect width="100" height="100" fill="#e37154" x="400" y="0" />
+	<rect width="100" height="100" fill="#dd4f32" x="500" y="0" />
+	<rect width="100" height="100" fill="#ce3928" x="600" y="0" />
+	<rect width="100" height="100" fill="#ab2a23" x="700" y="0" />
+	<rect width="100" height="100" fill="#892423" x="800" y="0" />
+	<rect width="100" height="100" fill="#6f211f" x="900" y="0" />
+</svg>
+
+<h2>Rob Roy</h2>
+<!-- 
+'rob-roy': {
+    '50': '#fdf9ed',
+    '100': '#f7edce',
+    '200': '#efd998',
+    '300': '#e8c56d',
+    '400': '#e0ab3f',
+    '500': '#d88e28',
+    '600': '#bf6d20',
+    '700': '#9f501e',
+    '800': '#823f1e',
+    '900': '#6b351c',
+},
+
+ -->
+<svg
+	width="1000"
+	height="100"
+	viewBox="0 0 1000 100"
+	fill="none"
+	xmlns="http://www.w3.org/2000/svg"
+>
+	<rect width="100" height="100" fill="#fdf9ed" x="0" y="0" />
+	<rect width="100" height="100" fill="#f7edce" x="100" y="0" />
+	<rect width="100" height="100" fill="#efd998" x="200" y="0" />
+	<rect width="100" height="100" fill="#e8c56d" x="300" y="0" />
+	<rect width="100" height="100" fill="#e0ab3f" x="400" y="0" />
+	<rect width="100" height="100" fill="#d88e28" x="500" y="0" />
+	<rect width="100" height="100" fill="#bf6d20" x="600" y="0" />
+	<rect width="100" height="100" fill="#9f501e" x="700" y="0" />
+	<rect width="100" height="100" fill="#823f1e" x="800" y="0" />
+	<rect width="100" height="100" fill="#6b351c" x="900" y="0" />
+</svg>
+
+<h2>Manz</h2>
+<!-- 
+'manz': {
+    '50': '#fafce9',
+    '100': '#f4f9ce',
+    '200': '#e7f3a3',
+    '300': '#d4e86d',
+    '400': '#bfda41',
+    '500': '#a1bf23',
+    '600': '#7d9917',
+    '700': '#5f7417',
+    '800': '#4c5c18',
+    '900': '#414f18',
+},
+
+ -->
+<svg
+	width="1000"
+	height="100"
+	viewBox="0 0 1000 100"
+	fill="none"
+	xmlns="http://www.w3.org/2000/svg"
+>
+	<rect width="100" height="100" fill="#fafce9" x="0" y="0" />
+	<rect width="100" height="100" fill="#f4f9ce" x="100" y="0" />
+	<rect width="100" height="100" fill="#e7f3a3" x="200" y="0" />
+	<rect width="100" height="100" fill="#d4e86d" x="300" y="0" />
+	<rect width="100" height="100" fill="#bfda41" x="400" y="0" />
+	<rect width="100" height="100" fill="#a1bf23" x="500" y="0" />
+	<rect width="100" height="100" fill="#7d9917" x="600" y="0" />
+	<rect width="100" height="100" fill="#5f7417" x="700" y="0" />
+	<rect width="100" height="100" fill="#4c5c18" x="800" y="0" />
+	<rect width="100" height="100" fill="#414f18" x="900" y="0" />
+</svg>
+
+<h2>Sulu</h2>
+<!-- 
+'sulu': {
+    '50': '#f1fce9',
+    '100': '#e1f9ce',
+    '200': '#c4f3a3',
+    '300': '#9de86d',
+    '400': '#7bda41',
+    '500': '#5bbf23',
+    '600': '#439917',
+    '700': '#357417',
+    '800': '#2d5c18',
+    '900': '#294f18',
+},
+
+ -->
+<svg
+	width="1000"
+	height="100"
+	viewBox="0 0 1000 100"
+	fill="none"
+	xmlns="http://www.w3.org/2000/svg"
+>
+	<rect width="100" height="100" fill="#f1fce9" x="0" y="0" />
+	<rect width="100" height="100" fill="#e1f9ce" x="100" y="0" />
+	<rect width="100" height="100" fill="#c4f3a3" x="200" y="0" />
+	<rect width="100" height="100" fill="#9de86d" x="300" y="0" />
+	<rect width="100" height="100" fill="#7bda41" x="400" y="0" />
+	<rect width="100" height="100" fill="#5bbf23" x="500" y="0" />
+	<rect width="100" height="100" fill="#439917" x="600" y="0" />
+	<rect width="100" height="100" fill="#357417" x="700" y="0" />
+	<rect width="100" height="100" fill="#2d5c18" x="800" y="0" />
+	<rect width="100" height="100" fill="#294f18" x="900" y="0" />
+</svg>
+
+<h2>Pastel Green</h2>
+<!-- 
+'pastel-green': {
+    '50': '#f0fdf0',
+    '100': '#ddfbde',
+    '200': '#bdf5c0',
+    '300': '#6de876',
+    '400': '#4eda59',
+    '500': '#26c132',
+    '600': '#1a9f25',
+    '700': '#187d21',
+    '800': '#18631e',
+    '900': '#16511c',
+},
+
+ -->
+<svg
+	width="1000"
+	height="100"
+	viewBox="0 0 1000 100"
+	fill="none"
+	xmlns="http://www.w3.org/2000/svg"
+>
+	<rect width="100" height="100" fill="#f0fdf0" x="0" y="0" />
+	<rect width="100" height="100" fill="#ddfbde" x="100" y="0" />
+	<rect width="100" height="100" fill="#bdf5c0" x="200" y="0" />
+	<rect width="100" height="100" fill="#6de876" x="300" y="0" />
+	<rect width="100" height="100" fill="#4eda59" x="400" y="0" />
+	<rect width="100" height="100" fill="#26c132" x="500" y="0" />
+	<rect width="100" height="100" fill="#1a9f25" x="600" y="0" />
+	<rect width="100" height="100" fill="#187d21" x="700" y="0" />
+	<rect width="100" height="100" fill="#18631e" x="800" y="0" />
+	<rect width="100" height="100" fill="#16511c" x="900" y="0" />
+</svg>
+
+<h2>Riptide</h2>
+<!-- 
+'riptide': {
+    '50': '#ecfdf4',
+    '100': '#d1fae2',
+    '200': '#a6f4ca',
+    '300': '#6de8ad',
+    '400': '#33d48c',
+    '500': '#0fba73',
+    '600': '#04975d',
+    '700': '#03794d',
+    '800': '#05603e',
+    '900': '#054f35',
+},
+
+
+ -->
+<svg
+	width="1000"
+	height="100"
+	viewBox="0 0 1000 100"
+	fill="none"
+	xmlns="http://www.w3.org/2000/svg"
+>
+	<rect width="100" height="100" fill="#ecfdf4" x="0" y="0" />
+	<rect width="100" height="100" fill="#d1fae2" x="100" y="0" />
+	<rect width="100" height="100" fill="#a6f4ca" x="200" y="0" />
+	<rect width="100" height="100" fill="#6de8ad" x="300" y="0" />
+	<rect width="100" height="100" fill="#33d48c" x="400" y="0" />
+	<rect width="100" height="100" fill="#0fba73" x="500" y="0" />
+	<rect width="100" height="100" fill="#04975d" x="600" y="0" />
+	<rect width="100" height="100" fill="#03794d" x="700" y="0" />
+	<rect width="100" height="100" fill="#05603e" x="800" y="0" />
+	<rect width="100" height="100" fill="#054f35" x="900" y="0" />
+</svg>
+
+<!-- 
+
+ https://uicolors.app/edit?sv1=cornflower-blue:50-f1f6fd/100-dfebfa/200-c6dcf7/300-9fc5f1/400-6da3e8/500-5086e1/600-3b6ad5/700-3257c3/800-2e489f/900-2a3f7e;cornflower-blue:50-eff2fe/100-e2e7fd/200-cbd2fa/300-abb5f6/400-898ef0/500-6f6de8/600-5d50db/700-5041c1/800-41379c/900-39337c;medium-purple:50-faf6fe/100-f2eafd/200-e8d9fb/300-d6bbf7/400-bc90f0/500-a76de8/600-8c45d8/700-7733bd/800-662f9a/900-54277c;lavender:50-fcf5fe/100-faeafd/200-f4d5f9/300-edb3f4/400-de6de8/500-d357de/600-b938c1/700-9c2ba0/800-812583/900-6c236c;carissma:50-fcf3f9/100-fae9f5/200-f8d2ec/300-f3aedb/400-e86dbb/500-e054a9/600-ce3489/700-b2246f/800-93215c/900-7b204f;deep-blush:50-fdf3f3/100-fce7ea/200-f8d3d9/300-f3aeba/400-e86d84/500-df5472/600-ca345b/700-aa264c/800-8f2245/900-7b2040;apricot:50-fdf4ef/100-fae4da/200-f3c7b5/300-e88e6d/400-e37154/500-dd4f32/600-ce3928/700-ab2a23/800-892423/900-6f211f;rob-roy:50-fdf9ed/100-f7edce/200-efd998/300-e8c56d/400-e0ab3f/500-d88e28/600-bf6d20/700-9f501e/800-823f1e/900-6b351c;manz:50-fafce9/100-f4f9ce/200-e7f3a3/300-d4e86d/400-bfda41/500-a1bf23/600-7d9917/700-5f7417/800-4c5c18/900-414f18;sulu:50-f1fce9/100-e1f9ce/200-c4f3a3/300-9de86d/400-7bda41/500-5bbf23/600-439917/700-357417/800-2d5c18/900-294f18;pastel-green:50-f0fdf0/100-ddfbde/200-bdf5c0/300-6de876/400-4eda59/500-26c132/600-1a9f25/700-187d21/800-18631e/900-16511c;riptide:50-ecfdf4/100-d1fae2/200-a6f4ca/300-6de8ad/400-33d48c/500-0fba73/600-04975d/700-03794d/800-05603e/900-054f35
+
+  -->
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,15 +1,20 @@
 <script>
-	import Fa from 'svelte-fa';
-	import { faFlag } from '@fortawesome/free-solid-svg-icons';
-	import { faGithub } from '@fortawesome/free-brands-svg-icons';
+	import Fa from "svelte-fa";
+	import { faFlag } from "@fortawesome/free-solid-svg-icons";
+	import { faGithub } from "@fortawesome/free-brands-svg-icons";
+	import Colors from "$lib/Colors.svelte";
 </script>
 
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 
-<div class="middle p-4 [&>*]:text-6xl">
+<div class="flex flex-row middle p-4 [&>*]:text-6xl">
 	<Fa icon={faFlag} />
 	<Fa icon={faGithub} />
+</div>
+
+<div>
+	<Colors />
 </div>
 
 <style>
@@ -22,6 +27,7 @@
 		height: 300px;
 		position: absolute;
 		left: 50%;
+		top: -10px;
 		transform: translateX(-50%);
 	}
 </style>
