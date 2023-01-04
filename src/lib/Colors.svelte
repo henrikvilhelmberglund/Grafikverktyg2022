edb13d8f63b5c6dfb7eb85752bd6d71fbdad1b9e
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-01-05 00:27:07 +0100

    feat: :sparkles: add color conversion functions

--- src/lib/Colors.svelte
+++ src/lib/Colors.svelte
@@ -1,6 +1,28 @@
 <script>
+	import { toHSL, toHex } from "$lib/colorConvert.js";
 	let colorArray = {
 		// 10 hexes per color
+		test: [
+			toHex(100, 50, 95) +
+				" " +
+				toHex(110, 50, 90) +
+				" " +
+				toHex(120, 50, 81) +
+				" " +
+				toHex(130, 50, 67) +
+				" " +
+				toHex(140, 50, 51) +
+				" " +
+				toHex(150, 50, 50) +
+				" " +
+				toHex(160, 50, 46) +
+				" " +
+				toHex(170, 50, 41) +
+				" " +
+				toHex(180, 50, 35) +
+				" " +
+				toHex(190, 50, 26)
+		],
 		indigo: ["#eef2ff #e0e7ff #c7d2fe #a5b4fc #818cf8 #6366f1 #4f46e5 #4338ca #3730a3 #312e81"],
 		violet: ["#f5f3ff #ede9fe #ddd6fe #c4b5fd #a78bfa #8b5cf6 #7c3aed #6d28d9 #5b21b6 #4c1d95"],
 		purple: ["#faf5ff #f3e8ff #e9d5ff #d8b4fe #c084fc #a855f7 #9333ea #7e22ce #6b21a8 #581c87"],
@@ -14,7 +36,7 @@
 		emerald: ["#ecfdf5 #d1fae5 #a7f3d0 #6ee7b7 #34d399 #10b981 #059669 #047857 #065f46 #064e3b"],
 		teal: ["#f0fdfa #ccfbf1 #99f6e4 #5eead4 #2dd4bf #14b8a6 #0d9488 #0f766e #115e59 #134e4a"]
 	};
-
+	console.log(toHex(100, 50, 50) + " " + toHex(110, 50, 50));
 	function outputUIColors(object) {
 		let outputColor = "";
 		let outputColors = "";
