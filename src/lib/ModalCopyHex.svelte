8f14b27dfc0dddb9d45134bad61e6bc972ed823c
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-01-06 19:36:33 +0100

    feat: :sparkles: add ModalCopyHex component for displaying a message when copying hex

---
+++ src/lib/ModalCopyHex.svelte
@@ -0,0 +1,32 @@
+<script>
+	export let modalHex;
+	import { fly } from "svelte/transition";
+
+	// https://developer.mozilla.org/en-US/docs/Web/API/Clipboard/write
+	function setClipboard(text) {
+		const type = "text/plain";
+		const blob = new Blob([text], { type });
+		const data = [new ClipboardItem({ [type]: blob })];
+
+		navigator.clipboard.write(data).then(
+			() => {
+				/* success */
+			},
+			() => {
+				/* failure */
+			}
+		);
+	}
+	setClipboard(modalHex);
+</script>
+
+<div
+	transition:fly={{ y: 100 }}
+	class="fixed bottom-12 left-1/2 bg-white p-8 rounded-3xl text-xl z-20 border-black border-2 shadow-black shadow-2xl">
+	<p>
+		Copied <span style="color: {modalHex}">{modalHex}</span> to clipboard!
+	</p>
+</div>
+
+<style>
+</style>
