223ef9e473cf58da1d39777a4e0c839628264327
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-01-06 22:50:16 +0100

    feat: :sparkles: use local storage store

--- src/lib/stores.js
+++ src/lib/stores.js
@@ -1,3 +1,5 @@
-import { writable } from "svelte/store";
+// import { writable } from "svelte/store";
+import { writable } from "svelte-local-storage-store";
 
-export const allColors = writable([]);
+// export const allColors = writable();
+export const allColors = writable("myColors", 0);
